b6976ef7dac7b711920121e829bf31ff15fa577a
Aryan <aryan191990@gmail.com>
2024-09-01 20:06:30 +0530

    Task 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { PipelineToolbar } from './toolbar';
 import { PipelineUI } from './ui';
 import { SubmitButton } from './submit';
+import { useStore } from './store';
+import { shallow } from 'zustand/shallow';
 
 function App() {
+  const { nodes, edges } = useStore(state => ({
+    nodes: state.nodes,
+    edges: state.edges,
+  }), shallow);
+
   return (
     <div className='bg-gradient-to-br from-[#1B0A42] to-black h-full pb-10'>
       <PipelineToolbar />
       <PipelineUI />
-      <SubmitButton />
+      <SubmitButton nodes={nodes} edges={edges} />
     </div>
   );
 }
--- src/submit.js
+++ src/submit.js
@@ -1,15 +1,46 @@
-// submit.js
+import React from 'react';
 
-export const SubmitButton = () => {
-    return (
-      <div className="flex items-center justify-center">
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition"
-        >
-          Submit
-        </button>
-      </div>
-    );
-  }
-  
+export const SubmitButton = ({ nodes, edges }) => {
+  const handleSubmit = async () => {
+    try {
+      console.log('Submitting data to backend...'); // for debug
+
+      const response = await fetch('http://127.0.0.1:8000/pipelines/parse', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          nodes: nodes.map(node => ({ id: node.id, type: node.type })),
+          edges: edges.map(edge => ({ source: edge.source, target: edge.target })),
+        }),
+      });
+
+      console.log('Response received:', response); // for debug
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const result = await response.json();
+
+      console.log('Result from backend:', result); // for edbug
+
+      alert(`Number of Nodes: ${result.num_nodes}, Number of Edges: ${result.num_edges}, Is DAG: ${result.is_dag}`);
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <button
+        type="button"
+        onClick={handleSubmit}
+        className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition"
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
